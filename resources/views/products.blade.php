feac37c730a1ef6693be609d6aa41b44c448ecba
Maurizio <maurizio.faedda1@gmail.com>
2021-01-19 16:51:54 +0100

    simple product printing

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,5 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Products</h1>
+    <div class="section-products">
+
+        @foreach ($dresses as $dress)
+            <div class="card">
+                <div class="dress-img">
+
+                </div>
+                <div class="dress-info">
+                    <h5>{{$dress->brand}}</h5>
+                    <h6>{{$dress->model}}</h6>
+                    <small>{{$dress->categories}} - {{$dress->color}}</small>
+                    <h6>{{$dress->price}} €</h6>
+                </div>
+            </div>
+        @endforeach
+
+    </div>
 @endsection
